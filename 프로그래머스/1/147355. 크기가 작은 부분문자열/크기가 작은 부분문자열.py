e3a49dcf65e480bac2d3ccf33ec409486af32187
JUNSEO PARK <50609962+HoyiTT@users.noreply.github.com>
2024-06-03 21:57:43 +0900

    Update 크기가 작은 부분문자열.py

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,5 +1,23 @@
 def split_string(input_string, length):
-    return [input_string[i:i+length] for i in range(len(input_string) - length + 1)]
+    """
+    입력 문자열을 지정된 길이만큼 잘라서 리스트로 반환합니다.
+
+    Args:
+        input_string (str): 잘라낼 원본 문자열.
+        length (int): 각 부분 문자열의 길이.
+
+    Returns:
+        list: 원본 문자열을 부분 문자열로 잘라 만든 리스트.
+    """
+    # 부분 문자열을 저장할 리스트 초기화
+    substrings = []
+
+    # 문자열의 각 위치에서 시작하여 지정된 길이만큼 부분 문자열을 생성
+    for i in range(len(input_string) - length + 1):
+        substring = input_string[i:i + length]
+        substrings.append(substring)
+
+    return substrings
 
 
 def solution(t, p):
