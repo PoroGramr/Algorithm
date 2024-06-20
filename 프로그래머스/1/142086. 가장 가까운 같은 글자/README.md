fe69b2f09dbc7750d98d12a88c8f9f0ff3561e15
JUNSEO PARK <50609962+HoyiTT@users.noreply.github.com>
2024-06-20 23:27:35 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.83 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 가까운 같은 글자 - 142086 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086) 
 
 ### 성능 요약
 
-메모리: 11.2 MB, 시간: 40.57 ms
+메모리: 11 MB, 시간: 1.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 18일 23:06:55
+2024년 06월 20일 23:27:32
 
 ### 문제 설명
 
--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,15 +1,11 @@
 def solution(s):
     """
-    입력으로 들어온 문자열을 하나하나 검색하면서 결과값 리스트에 저장하는 방식으로 구현할 예정
-    알파벳별로 리스트를 만들어 구현
+    딕셔너리를 이용하여 필요한 문자만 이용하여 출력값을 계산
     처음 들어온 문자는 -1을 append후, 문자열의 몇번째 요소인지 기록
     2번 이상 들어온 문자는 이전에 기록된 데이터를 바탕으로 다시 연산 후 append
     """
-    #알파벳 별 넘버링을 하기 위해 만든 리스트
-    alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    
-    # 각 알파벳별 카운트를 위해 만든 리스트
-    alphabetCount=[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+    # 딕셔너리 선언
+    count = dict()
     
     #result 리턴용 리스트 생성
     answer = []
@@ -17,26 +13,23 @@ def solution(s):
     # 입력 문자열을 한 글자씩 리스트로 생성
     inputString = list(s)
     
-    print(inputString)
     
     # 입력 문자열이 한 글자씩 저장된 리스트를 0번쨰 요소부터 마지막 요소까지 하나씩 검사
     for i in range(len(inputString)):
-        for a in range(0,26): # 모든 알파벳 검사를 위한 for문
-            # 처음들어온 문자를 분류하는 for문
-            if inputString[i] == alphabet[a] and alphabetCount[a] == -1:
-                # answer에 -1을 append한다.
-                answer.append(alphabetCount[a])
-                # 현재 문자의 위치값을 업데이트
-                alphabetCount[a] = i
-                continue
-                ## 두번 이상 만나는 문자를 분류하는 for문
-            elif inputString[i] == alphabet[a] and alphabetCount[
-                a] != -1:
-                # 같은 문자가 앞에서 얼마나 가까이에 위치했는지 계산
-                nearAlphabetCount = i - alphabetCount[a]
-                # answer에 nearAlphabetCount append
-                answer.append(nearAlphabetCount)
-                # 현재 문자의 위치값을 업데이트
-                alphabetCount[a] = i
-                continue
+        
+        #문자가 count라는 딕셔너리에 존재할 경우(이전에 한번 나왔을 경우))
+        if inputString[i] in count:
+            
+            # 거리의 차이를 계산하여 결과값 리스트에 저장
+            answer.append(i - count[inputString[i]])
+            
+        # 문자가 처음 등장했을 경우    
+        else:
+            
+            # -1을 결과값 리스트에 저장
+            answer.append(-1)
+            
+        # 등장한 문자의 위치를 dict에 저장(추후 거리 계산을 위해 사용)
+        count[inputString[i]] = i
+        
     return answer
