8fce7da21a59614eb22b228eb7c8231ef607ffe1
JUNSEO PARK <pjs9177@naver.com>
2024-08-23 19:42:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,14 @@
 def solution(absolutes, signs):
     answer = 0
-    for i in range(len(absolutes)):
-        if signs[i] == True:
-            answer += absolutes[i]
+    # for i in range(len(absolutes)):
+    #     if signs[i] == True:
+    #         answer += absolutes[i]
+    #     else:
+    #         answer -= absolutes[i]
+            
+    for value, sig in zip(absolutes, signs):
+        if sig:
+            answer += value
         else:
-            answer -= absolutes[i]
+            answer -= value
     return answer
