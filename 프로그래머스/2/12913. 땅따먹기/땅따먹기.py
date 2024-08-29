bbefeb308bf933678d3d906f9e403c31ebe281f6
JUNSEO PARK <pjs9177@naver.com>
2024-08-29 19:59:04 +0900

    [level 2] Title: 땅따먹기, Time: 400.54 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,19 @@
+def solution(land):
+    answer = 0
+    
+    memo = []
+    for i in range(len(land)):
+        memo.append([0] * 4)
+        
+    memo[0] = land[0]
+    # [[1, 2, 3, 5], [0, 0, 0, 0], [0, 0, 0, 0]]
+    
+    for i in range(1, len(land)):
+        for j in range(4):
+            cols = [memo[i-1][k] for k in range(4) if k != j]
+            memo[i][j] = land[i][j] + max(cols)
+            
+    answer = max(memo[-1])        
+        
+    
+    return answer
