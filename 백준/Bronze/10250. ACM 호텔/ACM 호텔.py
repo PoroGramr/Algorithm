c8ff4d9df9ce26fed116d79e3fd0c43edb032364
JUNSEO PARK <pjs9177@naver.com>
2024-09-11 23:08:57 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -11,27 +11,29 @@
 102호 보다는 301호를 더 선호
 """
 
-
 n = int(input())
 for _ in range(n):
   H, W, N = map(int, input().split())
-  
+  # 손님 객실의 층은 손님입장번호 % 층 
   floor = N % H
+
+  # 손님 객실의 방번호는 손님입장번호 // 층 + 1
   room = N // H + 1
 
+  # 하지만 층이 0이 나오는경우가 있는데 손님입장번호 % 층 == 0 일 경우에는 예외 케이스
   if floor == 0:
     floor = str(H)
     room = N // H
   
- 
-
   answer = ""
   answer += str(floor)
-  
+
+  # 호실이 한자릿수면 0을 추가
   if room < 10:
     answer += "0"
     answer += str(room)
   else:
     answer += str(room)
+
   print(answer)
   
