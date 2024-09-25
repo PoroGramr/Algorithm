3a392a18e051064cd0786e6f7e861eb3d270627e
JUNSEO PARK <pjs9177@naver.com>
2024-09-25 13:54:33 +0900

    [D3] Title: [Professional] 쥬스 나누기, Time: 259 ms, Memory: 51,184 KB -BaekjoonHub

---
+++ SWEA/D3/5601. ［Professional］ 쥬스 나누기/［Professional］ 쥬스 나누기.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n):
+  man = int(input())
+  print(f"#{i+1} ", end='')
+  for k in range(man):
+    print("1/",end="")
+    print(man, end=" ")
+  print("")
+    
