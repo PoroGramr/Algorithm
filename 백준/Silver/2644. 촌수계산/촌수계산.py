bd359e8510ca16b55bb3323a066d34a98211b46c
JUNSEO PARK <pjs9177@naver.com>
2024-09-27 19:29:45 +0900

    [Silver II] Title: 촌수계산, Time: 108 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -7,8 +7,11 @@ def bfs(s,e):
     q.append(s)
     while q:
         fa = q.popleft()
+        
+        # 만약 검사할 요소가 도착지접이라면
         if fa == e:
             return v[e] - 1
+        
         for fam in data[fa]:# 가족의 멤버
             if v[fam] == 0:
                 q.append(fam)
@@ -30,5 +33,7 @@ for _ in range(c):
     a,b = map(int, input().split())
     data[a].append(b)
     data[b].append(a)
+
+# bfs의 리턴값은 촌수를 리턴함
 answer = bfs(s,e)
 print(answer)
