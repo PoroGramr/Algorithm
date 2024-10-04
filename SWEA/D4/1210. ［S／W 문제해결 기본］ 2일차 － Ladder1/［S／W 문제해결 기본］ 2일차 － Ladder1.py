e2c14efc19a875d93fe5f3facdaa640471ce9710
JUNSEO PARK <pjs9177@naver.com>
2024-10-04 16:23:39 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 287 ms, Memory: 52,984 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -5,22 +5,24 @@ def dfs(y,x):
         q = deque()
         q.append((y,x))
         v[y][x] = 1
+        
         while q:
             cy,cx = q.popleft()
-            
-            
-            
+        
+            # 사다리타기의 우선순위인 좌우 -> 아래 순으로 진행
             for py,px in ((0,1),(0,-1),(1,0)):
                 ny ,nx = cy + py, cx + px
                 if 0 <= ny < 100 and 0 <= nx <100:
+                    
+                    # 만약 다음 좌표의 데이터 값이 2라면 True를 리턴
                     if data[ny][nx] == 2:
                         return True
-
+					
+                    # 계속해서 dfs 실행
                     if data[ny][nx] == 1 and v[ny][nx] == 0:
-
                         q.append((ny,nx))
-
                         v[ny][nx] = 1
+                        #
                         break
         return False
 
@@ -33,10 +35,15 @@ for I in range(1,11):
         data.append(list(map(int,input().split())))
         
     answer = 0
+    
+    # 0,0 ~ 0,100 까지 dfs를 실행
     for x in range(0,100):
         if data[0][x] == 1:
+            
+            # dfs를 실행하며 데이터 리스트에서 2를 만난다면 True를 리턴
+            # 해당 x 값이 정답
             if dfs(0,x):
                 answer = x
-                break
+                
     print(f"#{I} {answer}")
             
