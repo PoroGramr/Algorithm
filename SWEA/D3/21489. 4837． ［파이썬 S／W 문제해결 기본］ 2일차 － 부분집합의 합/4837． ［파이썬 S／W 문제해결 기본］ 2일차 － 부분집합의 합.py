f69528c6c33d27822f049141a42d12196c7a335a
JUNSEO PARK <pjs9177@naver.com>
2024-10-06 20:02:01 +0900

    [D3] Title: 4837. [파이썬 S/W 문제해결 기본] 2일차 - 부분집합의 합, Time: 188 ms, Memory: 51,892 KB -BaekjoonHub

--- SWEA/D3/21489. 4837． ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/4837． ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
+++ SWEA/D3/21489. 4837． ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/4837． ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
@@ -1,28 +1,24 @@
-
-def dfs(n ,sm, cnt):
-    global ans
-
-    # 종료 조건: n에 관련된 수식
-    if n == N:
-        if sm == K and cnt == CNT:
-            ans += 1
+def dfs(ind, n,sm): # ind : 리스트 인덱스 , n : 원소의 수, k : 합
+    global answer
+    if ind == 12:
+        if n == N and sm == K:
+            answer += 1
         return
-
-    # 숫자를 사용하는 경우
-    dfs(n+1, sm + data[n], cnt + 1)
-    # 사용하지 않는 경우
-    dfs(n+1, sm, cnt)
     
+    # 더하는 경유
+    dfs(ind+1, n + 1, sm + data[ind])
+
+    # 더하지 않는 경우
+    dfs(ind+1, n, sm)
+
+
 T = int(input())
 
-
-
-for i in range(1, T + 1):
-    CNT , K = map(int, input().split())
+for i in range(1,T+1):
+    N, K = map(int, input().split()) # N : 원소의 수 , K : 부분 집합의 합
     data = [n for n in range(1,13)]
-    N = 12
 
-    ans = 0
-    dfs(0, 0, 0)
+    answer = 0
+    dfs(0,0,0)
 
-    print(f"#{i} {ans}")
+    print(f"#{i} {answer}")
