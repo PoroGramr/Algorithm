ae911e9120387100dd9d963e40ac688871b9db29
JUNSEO PARK <pjs9177@naver.com>
2024-10-09 23:42:53 +0900

    Update answer.py

--- SWEA/Unrated/5208. [파이썬 SW 문제해결 구현] 5일차 - 전기버스2/answer.py
+++ SWEA/Unrated/5208. [파이썬 SW 문제해결 구현] 5일차 - 전기버스2/answer.py
@@ -1,22 +1,56 @@
-def dfs(n, cnt, sm):
+"""
+문제 이해
+    - 입력 : 테스트 케이스 T
+        테스트 케이스 별로 한 줄에 정류장 수 N개의 정류장 별 태러티 용량이 주어짐
+        ex)
+            3
+            5 2 3 1 1
+            10 2 1 3 2 2 5 4 2 1
+            10 1 1 2 1 2 2 1 2 1
+
+    - 출력 : 테스트 케이스 별 목적지에 도착하는데 필요한 최소한의 교환횟수를 출력
+!주의사항
+1. 출발지에서의 배터리 장착은 교환횟수에서 제외
+
+아이디어
+dfs(idx, cnt, bat) # 정류장 인덱스, 교체 횟수 카운트, 현재 전지량
+
+if idx > len(data) and bat >= 0:
+    ans = min(ans, cnt)
+
+# 해당 인덱스에서 배터리를 충전
+dfs(idx+1, cnt + 1, bat - 1 + data[idx])
+
+# 해당 인덱스에서 배터리를 충전하지 않음
+dfs(idx + 1, cnt, bat - 1)
+"""
+
+def dfs(idx, cnt, bat):
     global ans
-    if ans<=cnt:
+    if ans <= cnt:
         return
- 
-    if n==N:
-        ans = min(ans, cnt)
+    
+    if idx == N:
+        ans = min(ans,cnt)
         return
- 
-    # 가지치기를 고려하는 겨우: 유망한 답이 먼저 나오는 방향으로 호출
-    if sm>0:                    # 교체하지 않는 경우
-        dfs(n+1, cnt, sm-1)
-    dfs(n+1, cnt+1, lst[n]-1)   # 교체하는 경우
- 
+
+    # 교체를 안할 수 있을 때만 안한다.
+    if bat > 0:
+        dfs(idx+1, cnt, bat - 1)
+
+    # 교체는 언제든 가능하기에 항상 가능
+    dfs(idx+1, cnt + 1, data[idx] - 1)
+
 T = int(input())
-for test_case in range(1, T + 1):
-    lst = list(map(int, input().split()))
-    N = lst[0]
-    ans = N     # 모든 정류장에서 교체할경우 N
- 
-    dfs(2, 0, lst[1]-1) # 1번 정류장에서는 교체회수X, 2번부터 진행
-    print(f'#{test_case} {ans}')
+
+for t in range(1, T+1):
+
+    data = list(map(int, input().split()))
+    N = data[0]
+
+    # 카운트 저장을 위한 변수 생성
+    ans = N # 가능한 카운트의 최대 값
+
+    dfs(2,0,data[1] - 1) # 인덱스, 충전하는 카운트, 매터리의 양
+
+    print(f"#{t} {ans}")
