5df3dae81efad81557f9093e43fd79dd24570ee6
JUNSEO PARK <pjs9177@naver.com>
2024-10-21 19:42:05 +0900

    [D4] Title: 염라대왕의 이름 정렬, Time: 4,090 ms, Memory: 82,140 KB -BaekjoonHub

--- SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.py
+++ SWEA/D4/7701. 염라대왕의 이름 정렬/염라대왕의 이름 정렬.py
@@ -3,13 +3,20 @@ t = int(input())
 for T in range(1, t+1):
     N = int(input())
     data = []
+    # 문자를 리스트로 입력받음
     for _ in range(N):
         data.append(input())
     
+    # set을 활용하여 중복을 제거
     data = set(data)
+
+    # 다시 리스트로 만들어 줌, 정렬을 위해 리스트로 바꾸어 주어야 함
     data = list(data)
+
+    # 길이순으로 정렬 후, 사전순으로 정렬
     data.sort(key = lambda x : (len(x), x))
-    
+
+    # 요구사항에 맞게 출력
     print(f"#{T}")
     for p in data:
         print(p)
