7e2a61e14f08a8dff396544b9b14ed5123b6290f
JUNSEO PARK <pjs9177@naver.com>
2024-10-21 17:27:04 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 125 ms, Memory: 46,076 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
@@ -2,45 +2,48 @@ from collections import deque
 
 def bfs(s):
     q = deque()
+    v = [0] * 101
     q.append(s)  # 시작점 s를 큐에 추가
-    visited = [False] * (len(nei))  # 방문 체크 리스트
-    visited[s] = True  # 시작점 방문 처리
 
-    large = s  # 가장 큰 번호를 저장할 변수
-    last_level = []  # 마지막 레벨의 노드들을 저장할 리스트
+    v[s] = 1
+    large = s  # large는 s부터 시작해야 함
 
     while q:
-        size = len(q)  # 현재 레벨의 노드 수
-        last_level = []  # 매번 레벨이 바뀔 때 노드를 초기화
+        n = q.popleft()
 
-        for _ in range(size):
-            n = q.popleft()
-            last_level.append(n)  # 현재 레벨의 노드를 저장
+        if v[large] < v[n] or (v[large] == v[n] and large < n):
+            large = n
+        
 
-            for p in nei[n]:  # 현재 노드의 자식 노드들 탐색
-                if not visited[p]:  # 방문하지 않은 노드만 큐에 추가
-                    q.append(p)
-                    visited[p] = True  # 방문 처리
+        for p in nei[n]:
+            if v[p] == 0:
+                q.append(p)
+                v[p] = v[n] + 1
 
-    # 마지막 레벨의 노드 중 가장 큰 번호를 반환
-    return max(last_level)
+    return large
+
 
 
 for t in range(1, 11):
 
     # 입력받는 데이터의 길이, 시작점
-    cul, st = map(int, input().split())
+    cul , st = map(int, input().split())
 
     # 입력받는 데이터 (from to from to....)
     data = list(map(int, input().split()))
 
-    # 가장 큰 값을 바탕으로 리스트 생성
+    # 가장 큰값
     big = max(data)
-    nei = [[] for _ in range(big + 1)]  # 인접 리스트 생성
 
-    # 연락망 데이터 입력
-    for i in range(0, len(data), 2):
-        nei[data[i]].append(data[i+1])
+    # 가장 큰 값을 바탕으로 리스트 생성
+    nei = [[] for _ in range(big + 1)]
+
+    for i in range(len(data)):
+        if i % 2 == 0:
+            continue
+        else:
+            nei[data[i-1]].append(data[i])
 
     ans = bfs(st)
+
     print(f"#{t} {ans}")
