bb53bcbb4544c378392b51180684cdc231c46330
JUNSEO PARK <pjs9177@naver.com>
2024-10-25 18:15:07 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 117 ms, Memory: 44,812 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.py
@@ -1,14 +1,27 @@
 T = 10
 
 for t in range(1, T+1):
+    # 테스트 케이스 번호
     a = int(input())
+
+    # 검색할 문자열
     ck = list(input())
+
+    # 문자열의 길이(추후 연산에 필요)
     ckL = len(ck)
+
+    # 총 문자열을 리스트로 입력받음
     data = list(map(str, input()))
+
+    # 출현 횟수 카운트
     ans  = 0
+
     for i in range(len(data) - 1):
+
+        # 검색할 문자열의 길이만큼 총 문자열을 처음부터 순회
         if data[i:i+ckL] == ck:
             ans += 1
+    
     print(f"#{t} {ans}")
 
 
