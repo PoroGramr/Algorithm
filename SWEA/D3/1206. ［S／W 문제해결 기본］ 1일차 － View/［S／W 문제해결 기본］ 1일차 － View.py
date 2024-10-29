576f41b94a974782398561c2e39411e004bce5d8
JUNSEO PARK <pjs9177@naver.com>
2024-10-29 18:14:21 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 126 ms, Memory: 48,672 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,12 +1,16 @@
-for i in range(10):
-  n = int(input()) # 건물개수
-  data = list(map(int, input().split()))
-  result = 0
-  for k in range(2, n - 2):
-    left1 = data[k] - data[k-1]
-    left2 = data[k] - data[k-2]
-    right1 = data[k] - data[k+1]
-    right2 = data[k] - data[k+2]
-    if left1 > 0 and left2>0 and right1>0 and right2 > 0:
-      result += min(left1,left2,right1,right2)
-  print(f"#{i+1} {result}")
+T = 10
+for t in range(1, T+1):
+	N = int(input())
+	data = list(map(int, input().split()))
+	ans = 0
+
+	for i in range(2, N - 2):
+		left1 = data[i-2]
+		left2 = data[i-1]
+		right1 = data[i+1]
+		right2 = data[i+2]
+
+		if left1 < data[i] and left2 < data[i] and right1 < data[i] and right2 < data[i]:
+			ans += data[i] - max(left1, left2, right2, right1)	
+    
+	print(f"#{t} {ans}")
