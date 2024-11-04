52845f8065523a95b36f564c909d64cccdc2b5af
JUNSEO PARK <pjs9177@naver.com>
2024-11-04 20:57:43 +0900

    [D3] Title: 햄버거 다이어트, Time: 745 ms, Memory: 57,664 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,37 +1,43 @@
 # import sys
 # sys.setrecursionlimit(10**6)  # 재귀 제한을 1,000,000으로 설정
 
-
-def dfs(ii, sumc, curt): # index of ingredint, sum of calrorie , curent taste
-    global maxTaste
+def dfs(i, score, cal): # 인덱스, 점수, 칼로리
+    global ans
     
-    # 재료의 끝까지 탐색했다면 
-    if ii == N:
-        if sumc <= L:
-            maxTaste = max(curt, maxTaste)
+    # 모든 요소를 다 탐색했을 때
+    if i == M:
+        
+        # 제한 칼로리를 넘지 않았다면
+        if cal <= L:
+            ans = max(ans, score)
         return
 
+    # 해당 인덱스의 재료를 추가하는 경우
+    dfs(i+1, score+data[i][0], cal + data[i][1])
     
+    # 해당 인덱스의 재료를 추가하지 않는 경우
+    dfs(i+1, score, cal)
 
-    # 만약 해당 재료를 추가한다면
-    dfs(ii+1,sumc + data[ii][1], curt + data[ii][0])
-
-    # 추가하지 않는다면
-    dfs(ii+1, sumc,curt)
+    return
 
 
 T = int(input())
 
-for i in range(1, T+1):
-    N, L = map(int, input().split()) # N : count of ingredient. L : calorie limit
+for t in range(1, T+1):
+    N, L = map(int, input().split()) # 재료의 수, 제한 칼로리
     data = []
+
+
     for _ in range(N):
-        ing, cal = map(int, input().split()) # ingredient, calorie
-        data.append([ing,cal])
-    maxTaste = 0
-    dfs(0,0,0) # index of ingredint, sum of calrorie , curent taste
-   
-    print(f"#{i} {maxTaste}")
+        score, cal  = map(int, input().split()) # 점수, 칼로리를 리스트에 삽입
+        data.append([score, cal])
+    
+    # 데이터의 길이를 변수로 저장
+    M = len(data)
 
+    ans = 0
+    
+    # 탐색
+    dfs(0,0,0) # 인덱스, 점수, 칼로리
 
-
+    print(f"#{t} {ans}")
