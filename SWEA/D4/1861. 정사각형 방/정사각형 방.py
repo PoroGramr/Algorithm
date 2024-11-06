3f687986327bfba4f0037a550b67c120730d0fe0
JUNSEO PARK <pjs9177@naver.com>
2024-11-06 23:14:08 +0900

    [D4] Title: 정사각형 방, Time: 903 ms, Memory: 52,492 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -1,42 +1,66 @@
+"""
+
+이중 for문으로 모든 점을 탐색
+좌표를 바탕으로 bfs
+
+"""
 from collections import deque
-
-def bfs(y, x):
-    if memo[y][x] != 0:
-        return memo[y][x]
-
-    q = deque([(y, x)])
-    visited = [(y, x)]
+def bfs(y,x):
+    q = deque()
+    q.append((y,x))
+    
+    # 이동 횟수 카운드
     count = 1
-
+    
     while q:
         cy, cx = q.popleft()
-        for dy, dx in ((0,1), (0,-1), (1,0), (-1,0)):
-            ny, nx = cy + dy, cx + dx
+
+        for py, px in ((0,-1), (0,1), (1,0), (-1,0)):
+            ny, nx = cy + py, cx + px
+            
+            # 이동이 가능할때 마다 이동횟수 카운트 1씩 증가
             if 0 <= ny < N and 0 <= nx < N and data[ny][nx] == data[cy][cx] + 1:
                 count += 1
-                q.append((ny, nx))
-                visited.append((ny, nx))
-
-    for vy, vx in visited:
-        memo[vy][vx] = count
-    return count
+                q.append((ny,nx))
+    
+    # 이동가능한 모든 경우의 수를 시행했을 경우 방의 숫자와 이동횟수를 리턴
+    return data[y][x], count
 
 T = int(input())
 
-for tc in range(1, T+1):
+for t in range(1, T+1):
     N = int(input())
-    data = [list(map(int, input().split())) for _ in range(N)]
-    memo = [[0] * N for _ in range(N)]
 
-    max_count = 0
-    max_start = N*N
+    data = []
 
-    for y in range(N):
-        for x in range(N):
-            if memo[y][x] == 0:
-                count = bfs(y, x)
-                if count > max_count or (count == max_count and data[y][x] < max_start):
-                    max_count = count
-                    max_start = data[y][x]
+    for _ in range(N):
+        data.append(list(map(int, input().split())))
+        
+    # 출력으로 쓸 방의 번호
+    roomNum = 0
+    
+    # 출력으로 쓸 이동횟수
+    count = 0
+    
+    # 모든 좌표를 탐색
+    for i in range(N):
+        for j in range(N):
+            
+            # bfs의 결과로 해당 방의 번호와 이동 횟수가 리턴
+            tRoom, tCount = bfs(i,j)
+            
+            # 만약 이동 횟수는 같은데 방번호가 더 작은 숫자라면
+            if tCount == count :
+                
+                # 출력으로 사용할 방 번호 변경
+                if tRoom < roomNum:
+                    roomNum = tRoom
+            
+            # 이동횟수가 더 많다면 출력으로 사용할 방 번호와 이동횟수를 변경
+            elif tCount > count:
+                roomNum = tRoom
+                count = tCount
 
-    print(f"#{tc} {max_start} {max_count}")
+
+
+    print(f"#{t} {roomNum} {count}")
