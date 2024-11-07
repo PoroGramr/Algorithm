df0e366faf5eb3f1896cb42215e18627b71108cc
JUNSEO PARK <pjs9177@naver.com>
2024-11-07 11:28:02 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 112 ms, Memory: 44,792 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.py
@@ -1,40 +1,38 @@
-for i in range(10):
-    m = int(input())
-    # 문자열을 리스트로 입력받음
-    data = list(input())
-    
-    # 스택을 이용하여 문제를 해결하기 위해 리스트를 뒤집음
-    data = data[::-1]
-    
-    # 스택 선언
+T = 10
+
+for t in range(1, T + 1):
+    M = int(input())
+    data = list(map(str, input()))
+
     stack = []
-    
-    # data의 모든 요소를 pop할때까지 반복
-    while data:
-        
-        # 반복문의 첫번째 루프에서는 단순히 data를 pop해서 stack에 append한다
-        if not stack:
-            stack.append(data.pop())
-            continue
-            
-        # 만약 stack의 top과 data의 top이 짝이라면 둘다 pop 해준다
+    for i in range(M):
+        if data[i] == "{" or data[i] == "[" or data[i] == "(" or data[i] == "<":
+            stack.append(data[i])
         else:
-            if (stack[-1] == "{" and data[-1] == "}" ) or (stack[-1] == "[" and data[-1] == "]" ) or (stack[-1] == "(" and data[-1] == ")" ) or (stack[-1] == "<" and data[-1] == ">" ):
-                stack.pop()
-                data.pop()
-             
-            # 짝이 아니라면 stack에 append
-            else:
-                stack.append(data.pop())
-    
-    answer = 0
-    
-    # 만약 짝이 맞지 않는다면
+            if data[i] == "}":
+                if stack[-1] == "{":
+                    stack.pop()
+                else:
+                    stack.append(data[i])
+            if data[i] == "]":
+                if stack[-1] == "[":
+                    stack.pop()
+                else:
+                    stack.append(data[i])
+            if data[i] == ")":
+                if stack[-1] == "(":
+                    stack.pop()
+                else:
+                    stack.append(data[i])
+            if data[i] == ">":
+                if stack[-1] == "<":
+                    stack.pop()
+                else:
+                    stack.append(data[i])
+
     if stack:
-        answer = 0
-    
-    # 모두 짝이 맞는다면
+        print(f"#{t} 0")
     else:
-        answer = 1
-    print(f"#{i+1} {answer}")                              
-                                                                                                  
+        print(f"#{t} 1")
+
+
