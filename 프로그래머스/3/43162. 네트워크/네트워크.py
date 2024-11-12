4e866aeb5556127de7481bd31f90f6d981b5a217
JUNSEO PARK <pjs9177@naver.com>
2024-11-12 13:08:59 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def bfs(i, computers, v):
+    q = deque()
+    q.append(computers[i])
+    v[i] = 1
+    
+    while q:
+        c = q.popleft()
+        
+        for n in range(len(c)):
+            if c[n] == 1:
+                if v[n] == 0:
+                    v[n] = 1
+                    q.append(computers[n])
+        
+
+def solution(n, computers):
+    global answer 
+    answer = 0
+    v = [0] * n
+    
+    for i in range(n):
+        if v[i] == 0:
+            bfs(i, computers,v)
+            answer += 1
+    return answer
+    
