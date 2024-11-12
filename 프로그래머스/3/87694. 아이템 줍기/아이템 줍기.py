bb7d873c90f3addeea3961d24edde4c96c651ef6
JUNSEO PARK <pjs9177@naver.com>
2024-11-12 21:05:32 +0900

    [level 3] Title: 아이템 줍기, Time: 2.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,41 +1,51 @@
 from collections import deque
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
-    graph = [[-1 for _ in range(102)] for _ in range(102)]
-    visited = [[1 for _ in range(102)] for _ in range(102)]
-    direction = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-    dq = deque()
     
-    for r in rectangle:
-        x1, y1, x2, y2 = map(lambda x: x*2, r)
-        for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-                # x1, x2, y1, y2는 테두리이므로 제외하고 내부만 0으로 채움
-                if x1 < i < x2 and y1 < j < y2:
-                    graph[i][j] = 0
-                # 다른 직사각형의 내부가 아니면서 테두리일 때 1로 채움
-                elif graph[i][j] != 0:
-                    graph[i][j] = 1                
-    # 반복문을 마치면 테두리는 1, 내부는 0, 외부는 -1이 될 것이다   
+    # 보다 명확한 경계선 처리를 위해 보드의 좌표를 2배로 함
+    # 101로 설정해도 정답은 출력되지만 안정적인 연산을 위해 넉넉히 102로 초기화
+    board = [[0] * 102 for _ in range(102)]
     
-    # 캐릭터와 아이템의 좌표도 2배씩 늘린다
-    cx, cy, ix, iy = 2*characterX, 2*characterY, 2*itemX, 2*itemY
     
-    dq.append((cx, cy))
-    
-    while dq:
-        x, y = dq.popleft()
+    # board의 경계선 처리
+    for x1,y1,x2,y2 in rectangle:
+        # 좌표도 똑같이 2배해줌
+        # 좌측하단 x, 좌측하단 y,  우측상단 x,  우측상단 y
+        x1, y1, x2, y2 = x1 * 2, y1 * 2, x2 * 2, y2 * 2
         
-        if x == ix and y == iy:
-        	# 답을 반환할 때 2로 나누어 반환해준다
-            answer = visited[x][y] // 2
-            break   
-        for k in range(4):
-            nx, ny = x + direction[k][0], y + direction[k][1]
+        # 경계선 처리
+        for i in range(y1, y2+1):
+            for j in range(x1, x2+1):
+                if x1 < j < x2 and y1 < i < y2:
+                    board[i][j] = -1
+                # 다른 직사각형의 내부가 아니면서 현재 직사각형의 테두리일 때 1로 채움
+                elif board[i][j] != -1:
+                    board[i][j] = 1
+        
+    # 캐릭터와 아이템의 좌표도 2배 처리
+    cX, cY, iX, iY = characterX * 2, characterY * 2, itemX * 2, itemY * 2
+        
+        
+    # 방문 체크 배열
+    v = [[0] * 102 for _ in range(102)]
+        
+    # bfs를 위한 큐 생성
+    q = deque()
+    q.append((cY, cX))
+    v[cY][cX] = 1
+        
+    while q:
+        cy, cx = q.popleft()
+        if cy == iY and cx == iX:
+            break
             
-            if graph[nx][ny] == 1 and visited[nx][ny] == 1:
-                visited[nx][ny] += visited[x][y]
-                dq.append((nx, ny))
-
+        for py, px in ((0,-1),(0,1),(1,0),(-1,0)):
+            ny, nx = cy + py, cx + px
+            
+            # 방문체크와 동시에 이동경로를 카운트
+            if 0 <= ny < 102 and 0 <= nx < 102 and board[ny][nx] == 1 and v[ny][nx] == 0:
+                q.append((ny,nx))
+                v[ny][nx] = v[cy][cx] + 1
+    
+    answer = v[iY][iX] // 2
     return answer
