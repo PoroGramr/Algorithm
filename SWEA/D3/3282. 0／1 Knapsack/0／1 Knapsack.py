13c0b731ce1f81c522a87b9ab621101b5bd6f9e9
JUNSEO PARK <pjs9177@naver.com>
2024-11-14 15:05:17 +0900

    [D3] Title: 0/1 Knapsack, Time: 267 ms, Memory: 54,328 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
@@ -1,23 +1,21 @@
 T = int(input())
-
 for t in range(1, T + 1):
-    # N: 아이템 수, K: 배낭의 최대 무게
     N, K = map(int, input().split())
-    items = []
+    data = []
 
-    # 각 아이템의 무게와 가치를 입력받기
     for _ in range(N):
-        weight, value = map(int, input().split())
-        items.append((weight, value))
+        v, c = map(int, input().split())
+        data.append((v, c))
 
-    # DP 배열 초기화
-    dp = [0] * (K + 1)
+    dp = [[0] * (K + 1) for _ in range(N + 1)]
 
-    # 각 아이템에 대해 DP 갱신
-    for weight, value in items:
-        # 무게 제한을 초과하지 않도록 역순으로 DP 업데이트
-        for w in range(K, weight - 1, -1):
-            dp[w] = max(dp[w], dp[w - weight] + value)
+    for i in range(1, N + 1):
+        for j in range(1, K + 1):
+            if data[i-1][0] <= j:
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j - data[i-1][0]] + data[i-1][1])
+            else:
+                dp[i][j] = dp[i-1][j]
+    print(f"#{t} {dp[N][K]}")
 
-    # 배낭에 담을 수 있는 최대 가치 출력
-    print(f"#{t} {dp[K]}")
+
+
