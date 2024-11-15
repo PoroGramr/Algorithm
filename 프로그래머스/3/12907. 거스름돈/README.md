3d59b7fd2ceffd39f4e83c1d9d53db3a94af6a74
JUNSEO PARK <pjs9177@naver.com>
2024-11-15 20:07:54 +0900

    [level 3] Title: 거스름돈, Time: 451.52 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/README.md
+++ 프로그래머스/3/12907. 거스름돈/README.md
@@ -1,6 +1,6 @@
 # [level 3] 거스름돈 - 12907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12907?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12907) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 29일 19:43:39
+2024년 11월 15일 20:07:51
 
 ### 문제 설명
 
--- 프로그래머스/3/12907. 거스름돈/거스름돈.py
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -1,3 +1,27 @@
+"""
+5, [    1,  2, 5]
+
+0:      1    
+1:      1
+2:      1
+3:      1 + 1 dp[3] += dp[1]
+4:      1 + 2 dp[4] += dp[2]
+5:      1 + 2 + 1 dp[5] +=dp[3]
+
+5원 = 3원만드는 방법 + 2원 만드는 방법
+for coins
+dp[i] = dp[i] + dp[i-coin]
+"""
 def solution(n, money):
     answer = 0
+    
+    memo = [0] * (n + 1)
+    
+    # 0원을 만드는 방법은 1가지
+    memo[0] = 1
+    
+    for coin in money:
+        for i in range(coin, n+1): # 1 ~ 5 2 ~ 5 5 ~ 5
+            memo[i] = memo[i] + memo[i - coin]
+    answer = memo[i] %1_000_000_007 
     return answer
