649803111a10630ab2dece256be5e04c7978f5f4
JUNSEO PARK <pjs9177@naver.com>
2024-11-15 16:23:19 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 1,400 ms, Memory: 55,868 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.py
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.py
@@ -1,40 +1,49 @@
-di = [0,-1,1,0,0]
-dj = [0,0,0,-1,1]
-tb = [0,2,1,4,3]
+py = [0,-1,1,0,0] # 0은 제외, 상하좌우
+px = [0,0,0,-1,1] # 0은 제외, 상하좌우
+tp = [0,2,1,4,3]
+
 
 T = int(input())
 
-for t in range(1, T+1):
-    # 한변의 있는 셀의 개수, 격리 시간, 미생물 군집의 개수
-    N, M, K = map(int, input().split())
+for t in range(1, T + 1):
+    N, M, K = map(int, input().split()) # 가로세로, 격리시간, 군집의 개수
 
-    # i,j, 미생물 수, 이동 방향
-    data = [list(map(int, input().split())) for _ in range(K)]
+    micro = []
+
+    for _ in range(K):
+        y,x,m,p = map(int, input().split()) # 세로, 가로, 미생물 수, 이동방향
+        micro.append([y,x,m,p])
+
+    # M 시간동안 격리
     for _ in range(M):
-        for i in range(len(data)):
-            data[i][0] = data[i][0] + di[data[i][3]]
-            data[i][1] = data[i][1] + dj[data[i][3]]
-            # 만약 경계선 이라면
-            if data[i][0] == 0 or data[i][0] == N - 1 or data[i][1] == 0 or data[i][1] == N -1:
-                # 군집의 수 절반
-                data[i][2] //= 2
+        # 이동 처리
+        i = 0
+        for i in range(len(micro)):
+            micro[i][0] = micro[i][0] + py[micro[i][3]]
+            micro[i][1] = micro[i][1] + px[micro[i][3]]
 
-                # 이동 방향 반대
-                data[i][3] = tb[data[i][3]]
+            # 소독약 처리
+            if micro[i][0] == 0 or micro[i][1] == 0 or micro[i][0] == N -1 or micro[i][1] == N - 1:
 
+                # 미생물 절반
+                micro[i][2] //= 2
+                # 방향 반대
+                micro[i][3] = tp[micro[i][3]]
 
-        data.sort(key = lambda x : (x[0],x[1], x[2]), reverse = True)
+        micro.sort(key = lambda x : (x[0], x[1], x[2]), reverse=True)
 
         i = 1
-        while i < len(data):
-            if data[i- 1][0:2] == data[i][0:2]:
-                data[i-1][2] += data[i][2]
-                data.pop(i)
+        while i < len(micro):
+            if micro[i-1][0:2] == micro[i][0:2]:
+                micro[i-1][2] += micro[i][2]
+                micro.pop(i)
             else:
                 i += 1
-
     ans = 0
-    for a in data:
+    for a in micro:
         ans += a[2]
     print(f"#{t} {ans}")
+
+
+
 
