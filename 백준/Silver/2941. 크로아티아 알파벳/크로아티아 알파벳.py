bf4feef9b3f147886c9373b5421e1d81d23d5742
JUNSEO PARK <pjs9177@naver.com>
2025-02-13 15:41:52 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,8 +1,32 @@
-cri = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj','s=', 'z=']
-s = input()
+"""
+예전에는 운영체제에서 크로아티아 알파벳을 입력할 수가 없었다. 따라서, 다음과 같이 크로아티아 알파벳을 변경해서 입력했다.
 
-for c in range(len(cri)):
-    s = s.replace(cri[c], str(c))
-print(len(s))
+크로아티아 알파벳	변경
+č	c=
+ć	c-
+dž	dz=
+đ	d-
+lj	lj
+nj	nj
+š	s=
+ž	z=
+예를 들어, ljes=njak은 크로아티아 알파벳 6개(lj, e, š, nj, a, k)로 이루어져 있다. 단어가 주어졌을 때, 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력한다.
 
+dž는 무조건 하나의 알파벳으로 쓰이고, d와 ž가 분리된 것으로 보지 않는다. lj와 nj도 마찬가지이다. 위 목록에 없는 알파벳은 한 글자씩 센다.
 
+"""
+
+croWord = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
+word = input()
+
+word = word.replace(croWord[0], '0')
+word = word.replace(croWord[1], '1')
+word = word.replace(croWord[2], '2')
+word = word.replace(croWord[3], '3')
+word = word.replace(croWord[4], '4')
+word = word.replace(croWord[5], '5')
+word = word.replace(croWord[6], '6')
+word = word.replace(croWord[7], '7')
+
+print(len(word))
