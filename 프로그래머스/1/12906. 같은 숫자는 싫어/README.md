fb9e3241b4d68cbf738080761a075fd75e8c507e
JUNSEO PARK <pjs9177@naver.com>
2025-05-19 23:54:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 116.68 ms, Memory: 27.2 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 27.8 MB, 시간: 69.53 ms
+메모리: 27.2 MB, 시간: 116.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 22일 15:31:24
+2025년 05월 19일 23:54:27
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,34 +1,11 @@
-"""
-문제이해
-- 0에서 9로 이루어진 배열이 주어짐
-- 연속된 숫자는 하나만 남기고 제거
-- 입력 : 숫자[리스트]
-- 출력 : 숫자-연속이제거된[리스트]
-
-아이디어
-- 스택 필요
-- arr을 순회
-    - arr에 요소를 스택에 push
-    - 스택에 현재 쌓여있는 요소의 값과 다르면 push
-    - peek != arr -> push
-
-제한사항
--
-"""
-
 def solution(arr):
-    # 스택을 선언
-    stack = []
-    
-    # arr을 순회하며 요소를 검사
-    for num in arr:
-        
-        # 만약 스택이 비워져있다면(맨 처음 for문 시작시)
-        if not stack:
-            stack.append(num)
-            
-        # 스택의 peek값과 num 값이 같지 않다면 push
-        elif stack[-1] != num:
-            stack.append(num)
-    
-    return stack
+    answer = []
+    for i in arr:
+        if len(answer) == 0:
+            answer.append(i)
+            continue
+        if answer[-1] != i:
+            answer.append(i)
+        else:
+            continue
+    return answer
