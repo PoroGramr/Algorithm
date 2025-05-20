16387b84c1f1fc0a5c3317b59841ca6eb8e0efa3
JUNSEO PARK <pjs9177@naver.com>
2025-05-20 17:58:36 +0900

    [level 2] Title: 타겟 넘버, Time: 344.31 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 4.76 ms
+메모리: 9.36 MB, 시간: 344.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 14일 16:59:39
+2025년 05월 20일 17:58:31
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,20 @@
-def dfs(numbers, target, i, sm):
-    global ans
+def dfs(numbers, target, i, currentSum):
+    global answer
     if i == len(numbers):
-        if sm == target:
-            ans += 1
-        return
+        if currentSum == target:
+            answer += 1
+        return        
     
-    dfs(numbers, target, i+1, sm + numbers[i])
+    dfs(numbers, target, i + 1, currentSum + numbers[i])
     
-    dfs(numbers, target, i+1, sm - numbers[i])
+    dfs(numbers, target, i + 1, currentSum - numbers[i])
     
-        
-
+    
+    
 
 def solution(numbers, target):
-    global ans
-    ans = 0
-    dfs(numbers,target, 0,0) # 인덱스, 지금까지의 합
-    return ans
+    global answer
+    answer = 0
+    
+    dfs(numbers, target, 0,0)
+    return answer
