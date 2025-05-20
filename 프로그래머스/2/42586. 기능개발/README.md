4e1e4b10986ab520eb68c44bf9d328791a3aece4
JUNSEO PARK <pjs9177@naver.com>
2025-05-20 09:26:53 +0900

    [level 2] Title: 기능개발, Time: 1.28 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.05 ms
+메모리: 9.24 MB, 시간: 1.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 22일 16:10:17
+2025년 05월 20일 09:26:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,58 +1,32 @@
 """
-문제 이해
-- list 2개가 주어짐 현재 작업된 퍼센트 리스트, 
-각 작업별 하루에 작업할 수 있는 퍼센트 리스트
-- 각 작업은 독립적으로 진행되지만 배포는 선행된 작업이 완료되어야 배포할 수 있음 
-
-- 입력 : 
-    - progresses : 작업의 진도가 적힌 정수 리스트
-    - speeds : 작업의 속도가 적힌 정수 리스트
-
-- 출력:
-    - 배포가 일어날 때 배포되는 기능의 수 리스트
-
-아이디어
-- 1기수마다 progresses의 현황을 업데이트 하기 (speeds 만큼)
-- 배포 가능한 기능 배포하기 (queue)
-    - 배포된 기능이 있다면 숫자세서 추가하기
-
-
-제한사항
-- n : 100 
 """
 
-from collections import deque # queue를 사용하기 위해 import
+from collections import deque 
 def solution(progresses, speeds):
-    
-    answer = []
-    
-    # 입력받은 2개의 리스트를 큐로 변환
     progresses = deque(progresses)
     speeds = deque(speeds)
+
+    answer = []
+
+    # 100 프로가 넘는 것들끼리 다시 카운팅
     
-    # progresses가 empty가 되버리면 중단
-    while progresses:
-        for update in range(len(progresses)):
-            
-            # speeds값만큼 progresses를 업데이트
-            progresses[update] += speeds[update]
-            
-            # 만약 100을 넘기면 100으로 고정
-            if progresses[update] > 100:
-                progresses[update] = 100
-            
-        # 배포 기능 계산용 변수 선언
-        cnt = 0
+    while(len(progresses) != 0):
+        # 하루씩 진행률 업데이트
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
         
-        # progresses가 비워져있지 않고 첫번째 요소가 100일때
-        while progresses and progresses[0] == 100 :
-            progresses.popleft()
-            speeds.popleft()
-            cnt += 1
-        
-        # cnt가 1 이상일때만 answer에 append
-        if cnt > 0: 
-            answer.append(cnt)
+        currentComplete = 0
+        print(progresses)
+        for j in range(len(progresses)):
+            currentProgress = progresses[0]
+            if currentProgress >= 100:
+                currentComplete += 1
+                progresses.popleft()
+                speeds.popleft()
+            else:
+                break
+        if currentComplete > 0:
+            answer.append(currentComplete)
         
     return answer
     
