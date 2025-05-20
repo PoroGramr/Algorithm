151f5f3680ad12d1bafa0410821c6b4ced58a519
JUNSEO PARK <pjs9177@naver.com>
2025-05-20 09:58:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.22 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,28 +1,27 @@
 def solution(genres, plays):
-    # 1. 장르별 총 재생 횟수 계산
-    genre_play_sum = {}
-    for g, p in zip(genres, plays):
-        genre_play_sum[g] = genre_play_sum.get(g, 0) + p
-
-    # 2. 장르별 곡 리스트 생성
-    songs_by_genre = {}
-    for idx, (g, p) in enumerate(zip(genres, plays)):
-        songs_by_genre.setdefault(g, []).append((idx, p))
-
-    # 3. 장르별 곡을 재생 수 내림차순, 인덱스 오름차순으로 정렬
-    for g in songs_by_genre:
-        songs_by_genre[g].sort(key=lambda x: (-x[1], x[0]))
-
-    # 4. 장르를 총 재생 횟수 내림차순으로 정렬
-    sorted_genres = sorted(genre_play_sum.keys(),
-                           key=lambda g: genre_play_sum[g],
-                           reverse=True)
-
-    # 5. 각 장르에서 상위 2곡씩 추출하여 답안 리스트 작성
+    genreDict = {}
+    
     answer = []
-    for g in sorted_genres:
-        top_two = songs_by_genre[g][:2]
-        for idx, _ in top_two:
-            answer.append(idx)
+    #1. 많이 재생된 장르 추출
+    for i in range(len(genres)):
+        genreDict[genres[i]] = genreDict.get(genres[i], 0) + plays[i]
+    
+    sortedDict = sorted(genreDict.items(), key = lambda x : -x[1] )
+    print(sortedDict)
+    #2. 장르 내에서 많이 재생된 노래 추출(고유 번호 : 재생된 수)
+    for a in range(len(sortedDict)):
+        popular = {}
+        for j in range(len(genres)):
+            if genres[j] == sortedDict[a][0]:
+                popular[j] = plays[j]
+
+        sortedPopular = sorted(popular.items(), key = lambda x : -x[1])
+
+        if (len(sortedPopular)) == 1:
+            answer.append(sortedPopular[0][0])
+        else:
+            for k in range(2):
+                answer.append(sortedPopular[k][0])
+    #3. 재생횟수가 같다면 고유 번호가 낮은 노래를 먼저 추출
 
     return answer
