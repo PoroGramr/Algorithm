e062c896ab28a8b524bb79ac8f5a5f91a8496617
JUNSEO PARK <pjs9177@naver.com>
2025-05-26 09:45:02 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 59.24 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,26 +1,24 @@
 def solution(gems):
-    N = len(gems)
-    answer = [0, N - 1]
-    kind = len(set(gems))
-    dic = {gems[0]:1,}
-    s,e = 0,0
-    
-    while s < N and e < N:
-        if(len(dic)) < kind:
-            e += 1
-            if e == N:
-                break
-            dic[gems[e]] = dic.get(gems[e], 0) + 1
-            
-        else:
-            if(e-s+1) < (answer[1] - answer[0] + 1):
-                answer = [s,e]
-            if dic[gems[s]] == 1:
-                del dic[gems[s]]
-            else:
-                dic[gems[s]] -= 1
-            s += 1
-    
-    answer[0] += 1
-    answer[1] += 1
-    return answer
+    kind = len(set(gems))           # 전체 보석 종류 수
+    counts = {}                     # 윈도우 안 보석별 개수 저장
+    answer = [0, len(gems) - 1]     # 최악의 구간 (0-based)
+    left = 0
+
+    for right, gem in enumerate(gems):
+        # ① counts.get 키 인자를 제대로 gems[right]로 수정
+        counts[gem] = counts.get(gem, 0) + 1
+
+        # ② 모든 종류가 포함되면
+        while len(counts) == kind:
+            # 더 짧은 구간인지 확인
+            if right - left < answer[1] - answer[0]:
+                answer = [left, right]
+
+            # left 보석 하나 제거
+            counts[gems[left]] -= 1
+            if counts[gems[left]] == 0:
+                del counts[gems[left]]
+            left += 1
+
+    # 1-based 인덱스로 변환하여 반환
+    return [answer[0] + 1, answer[1] + 1]
