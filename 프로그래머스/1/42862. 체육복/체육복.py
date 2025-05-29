2012a63f11947d87279f0e34ffd6bc62dd21eb19
JUNSEO PARK <pjs9177@naver.com>
2025-05-29 11:40:38 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,20 +1,22 @@
 def solution(n, lost, reserve):
+    # 현재 체육복이 있는 학생 수
     lost.sort()
     reserve.sort()
-    answer = n - len(lost)
-    for i in range(1, n+1):
-        if i in lost and i in reserve:
-            reserve.remove(i)
-            lost.remove(i)
-            answer += 1
+    lostSet = set(lost)
+    reserveSet = set(reserve)
     
-    for i in lost:        
-        if (i - 1) in reserve:
-            reserve.remove(i-1)
-            answer += 1
-            continue
-        elif (i + 1) in reserve:
-            reserve.remove(i+1)
-            answer += 1
-        
+    both = lostSet & reserveSet
+    lostSet -= both
+    reserveSet -= both
+    
+    for stu in sorted(lostSet):
+        if (stu - 1) in reserveSet:
+            lostSet.remove(stu)
+            reserveSet.remove(stu-1)
+        elif (stu + 1) in reserveSet:
+            lostSet.remove(stu)
+            reserveSet.remove(stu+1)
+    
+    answer = n - len(lostSet)
+    
     return answer
