c5f25e279e6860b8242e218756a44983fef192e9
JUNSEO PARK <pjs9177@naver.com>
2025-06-11 10:29:38 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1720 ms, Memory: 101428 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,31 +1,40 @@
-import sys
 from collections import deque
+import sys
 
 input = sys.stdin.readline
 
+# N : 도시의 개수, M : 도로의 개수, K : 원하는 최단 거리 정보, X : 출발 도시의 번호
 N, M, K, X = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-distance = [-1] * (N + 1)  # 거리 초기화
+
+data = [[] for _ in range(N + 1)]
+v = [-1 for _ in range(N + 1)]
 
 for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
+    s, e = map(int, input().split())
+    data[s].append(e)
 
-# BFS
-q = deque([X])
-distance[X] = 0
 
+q = deque()
+q.append(X)
+v[X] = 0
 while q:
-    current = q.popleft()
-    for next_node in graph[current]:
-        if distance[next_node] == -1:
-            distance[next_node] = distance[current] + 1
-            q.append(next_node)
+    cur = q.popleft()
+    for next in data[cur]:
+        if v[next] == -1:
+            q.append(next)
+            v[next] = v[cur] + 1
 
-# 정답 찾기
-answer = [i for i, dist in enumerate(distance) if dist == K]
+answer = []
+
+for check in range(len(v)):
+    if v[check] == K:
+        answer.append(check)
+
 
 if answer:
-    print('\n'.join(map(str, sorted(answer))))
+    for i in answer:
+        print(i)
 else:
-    print(-1)
+    print("-1")
+
+
