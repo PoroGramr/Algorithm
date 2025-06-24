83b1388ab611e4fd5d8d329ddf6fc1429c2e62bd
JUNSEO PARK <pjs9177@naver.com>
2025-06-24 16:55:36 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 251.31 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,21 +1,20 @@
 def solution(cap, n, deliveries, pickups):
     answer = 0
-
+    
     d = 0
     p = 0
-
+    
     for i in range(n-1, -1, -1):
-
-        cnt = 0
-
+        
         d -= deliveries[i]
         p -= pickups[i]
-
+        
+        cnt = 0
         while d < 0 or p < 0:
             d += cap
             p += cap
             cnt += 1
-
-        answer += (i + 1) * 2 * cnt
+        
+        answer += (i + 1) * cnt * 2
 
     return answer
