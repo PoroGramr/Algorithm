7ad410ad2fddb12030f990e7890e047582f350f0
JUNSEO PARK <pjs9177@naver.com>
2025-08-28 14:55:03 +0900

    [level 2] Title: 더 맵게, Time: 1955.13 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,44 +1,19 @@
-"""
-- newScov = mini + (min2 * 2)
-- 모든 음식의 scov >= k 
-- 몇 번이나 음식 조합을 반복해야할까
-
-- 입력 : scoville 음식 리스트 / k 최소 스코빌지수
-- 출력 : 모든 음식이 k 이상이 되기 위한 조합 횟수
-
-반복 : 모든 음식이 k보다 클 때 까지
-    - 음식 리스트를 정렬 -> min1, min2 찾기
-    - min1 < k
-    - newScov = mini + (min2 * 2)
-    - newScv -> 음식 리스트
-    
-항상 정렬하는 대신 -> 음식 리스트르 heap으로 유지해서 min을 빠르게 찾아내자
-
-종료 조건 : len(scoville) == 1
-    => -1 출력
-
-n : 1M -> 힙 써야함
-"""
-
 import heapq
 
 def solution(scoville, K):
+    heapq.heapify(scoville)
     answer = 0
-    heap = []
     
-    for scov in scoville:
-        heapq.heappush(heap,scov)
-    
-    while heap[0] < K:
-        if len(heap) == 1:
-            return -1
-        min1 = heapq.heappop(heap)
-        min2 = heapq.heappop(heap)
+    while scoville[0] < K:
+        if len(scoville) <= 1:
+            answer = -1
+            break
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
         
-        mix = min1 + (min2 * 2)
+        new = min1 + (min2 * 2)
         
-        heapq.heappush(heap,mix)
-        
+        heapq.heappush(scoville, new)
         answer += 1
-    
+        
     return answer
