3854baafe9b59b5ca49552845f81d049b532aecc
JUNSEO PARK <pjs9177@naver.com>
2025-09-01 20:24:34 +0900

    [level 1] Title: 최소직사각형, Time: 5.42 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,23 +1,18 @@
-"""
-1. 가로형 명함과 세로형 명함의 타입을 통일 시킨다.
-가로형 명함: w > h
-세로형 명함: h < w
-
-2. 전체 명함의 최대 가로 * 최대 세로 -> 지갑의 크기를 계산한다.
-"""
-
 def solution(sizes):
     answer = 0
+    wMax = 0
+    hMax = 0
+    nSizes = []
+    for w,h in sizes:
+        tmp = [w,h]
+        tmp.sort()
+        nSizes.append(tmp)
     
-    max_width = -1
-    max_height = -1
+    for w,h in nSizes:
+        wMax = max(wMax,w)
+        hMax = max(hMax,h)
     
-    for s in sizes:
-        s.sort() 
-        
-        max_width = max(max_width, s[1])
-        max_height = max(max_height, s[0])
-        
-    answer = max_width*max_height
-        
+    answer = wMax * hMax
+    
+    
     return answer
