69c21835daf08e1a6626fbad7dc135d72497c526
JUNSEO PARK <pjs9177@naver.com>
2025-09-23 20:45:09 +0900

    [level 3] Title: 단속카메라, Time: 2.64 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,18 +1,23 @@
 def solution(routes):
-    
+
     routes.sort(key = lambda x : (x[1], x[0]))
     
-    print(routes)
-            
+    # 첫번째 차 진출 지점 기준으로 연산 시작        
     answer = 1
     lastCam = routes[0][1]
+    
+    
     for i in range(1, len(routes)):
         s = routes[i][0]
         e = routes[i][1]
         
+        # 만약 이전 진출 차량과 이동 경로 겹치는 부분이 있다면 카메라수 추가 x
         if s <= lastCam and e >= lastCam:
             continue
+            
+        # 이전 진출 차량과 이동 경로가 겹치지 않는다면 카메라수 추가 O, 새로운 카메라 위치 설정
         else:
             answer += 1
             lastCam = e
+        
     return answer
