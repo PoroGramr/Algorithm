7017b14a86f52b33f8805688f043f55b1191f6d2
JUNSEO PARK <pjs9177@naver.com>
2025-09-26 21:45:47 +0900

    [level 3] Title: 베스트앨범, Time: 0.27 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -7,16 +7,19 @@ def solution(genres, plays):
         genreDict[genres[i]] = genreDict.get(genres[i], 0) + plays[i]
     
     sortedDict = sorted(genreDict.items(), key = lambda x : -x[1] )
+    
     print(sortedDict)
+    
     #2. 장르 내에서 많이 재생된 노래 추출(고유 번호 : 재생된 수)
     for a in range(len(sortedDict)):
         popular = {}
         for j in range(len(genres)):
             if genres[j] == sortedDict[a][0]:
                 popular[j] = plays[j]
-
+        
+        print(popular)
         sortedPopular = sorted(popular.items(), key = lambda x : -x[1])
-
+        print(sortedPopular)
         if (len(sortedPopular)) == 1:
             answer.append(sortedPopular[0][0])
         else:
