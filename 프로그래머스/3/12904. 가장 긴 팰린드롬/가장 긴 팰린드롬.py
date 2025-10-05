b278aed578f1287956f1ff06970c81eab4c2c5a2
JUNSEO PARK <pjs9177@naver.com>
2025-10-05 15:14:42 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 2908.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,19 +1,7 @@
 def solution(s):
-    n = len(s)
-    if n <= 1:
-        return n
-    
-    def expand(l, r):
-        while l >= 0 and r < n and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return r - l - 1  # 마지막 실패 한 번 전 길이
-    
-    ans = 1
-    for i in range(n):
-        # 홀수 중심
-        ans = max(ans, expand(i, i))
-        # 짝수 중심
-        ans = max(ans, expand(i, i+1))
-        
-    return ans
+    answer = 0
+    for i in range(len(s)):
+        for j in range(i+1, len(s)+1):
+            if s[i:j] == s[i:j][::-1]:
+                answer = max(answer, len(s[i:j]))
+    return answer
