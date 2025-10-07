ed064696a594c3ee5f29bad232c52cf225b32109
JUNSEO PARK <pjs9177@naver.com>
2025-10-07 11:12:04 +0900

    [level 2] Title: 숫자의 표현, Time: 6.32 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -2,12 +2,12 @@ def solution(n):
     answer = 0
     
     for i in range(1, n+1):
-        sum_val = 0
-        for j in range(i, n+1):
-            sum_val += j
-            if sum_val == n:
+        sumVal = 0
+        for j in range(i, n + 1):
+            sumVal += j
+            
+            if sumVal == n:
                 answer += 1
-            elif sum_val > n:
+            elif sumVal > n:
                 break
-                
     return answer
