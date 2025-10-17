fb142b1b29bc0627879eedde5596106ed797a1ce
JUNSEO PARK <pjs9177@naver.com>
2025-10-17 09:08:07 +0900

    [level 3] Title: 정수 삼각형, Time: 43.29 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 19.1 MB, 시간: 42.07 ms
+메모리: 18.4 MB, 시간: 43.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 29일 20:50:53
+2025년 10월 17일 09:08:02
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -4,22 +4,23 @@
 - memo[-1][0]
 """
 def solution(triangle):
+    
     n = len(triangle) # 삼각형의 층수
     
     memo = []
     
-    for i in range(n):
-        memo.append([-1] * len(triangle[i]))
-    # 	[[-1], [-1, -1], [-1, -1, -1], [-1, -1, -1, -1], [-1, -1, -1, -1, -1]]
-    
+    for tri in triangle:
+        memo.append(list(-1 for _ in range(len(tri))))
+        
     triangle.reverse()
     memo.reverse()
     
     memo[0] = triangle[0]
+
+    for y in range(1, n):
+        for x in range(len(triangle[y])):
+            memo[y][x] = max(memo[y-1][x], memo[y-1][x+1]) + triangle[y][x]
     
-    for i in range(1,n):
-        for j in range(len(triangle[i])):
-            memo[i][j] = max(memo[i-1][j], memo[i-1][j+1]) + triangle[i][j]
-            
     answer = memo[-1][0]
+    
     return answer
