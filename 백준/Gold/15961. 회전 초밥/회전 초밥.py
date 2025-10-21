b316ceefef353a1568d0dcc8bbf0af59e7ac4632
JUNSEO PARK <pjs9177@naver.com>
2025-10-21 14:54:36 +0900

    [Gold IV] Title: 회전 초밥, Time: 1040 ms, Memory: 135840 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.py
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.py
@@ -1,18 +1,20 @@
-import sys
+"""
+1. 벨트의 임의의 한 위치부터 K개의 접시를 연속해서 먹을 경우 할이낙격
+2. 각 고객에게 초밥의 종류 하나가 쓰인 쿠폰을 발행, 1번 행사에 참여했을 경우 초밥 하나를 무료로 제공,
+만약 해당 초밥이 벨트에 없다면 새로 만들어 제공
 
-input = sys.stdin.readline
 
+"""
+#  접시의 수 N, 초밥의 가짓수 d, 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
 n, d, k, c = map(int, input().split())
 belt = [int(input()) for _ in range(n)]
 
-count = [0] * (d + 1)   # 각 초밥 번호의 개수
+count = [0] * (d + 1)
 unique = 0
 
-# 쿠폰 초밥을 미리 먹었다고 가정(중복 허용) → 항상 쿠폰 고려
 count[c] += 1
 unique += 1
 
-# 초기 윈도우 [0..k-1]
 for i in range(k):
     if count[belt[i]] == 0:
         unique += 1
@@ -20,21 +22,22 @@ for i in range(k):
 
 answer = unique
 
-# 슬라이딩: 시작 위치 1..n-1 (원형이므로 % n)
 for i in range(1, n):
-    # 왼쪽 제거
     left = belt[i - 1]
     count[left] -= 1
     if count[left] == 0:
         unique -= 1
-
-    # 오른쪽 추가
-    right = belt[(i + k - 1) % n]
+    
+    right = belt[(i +k - 1) % n]
     if count[right] == 0:
         unique += 1
     count[right] += 1
-
+    
     if unique > answer:
         answer = unique
-
+    
+    
 print(answer)
+
+
+
