61def533fcafc95aa9376dbc5a7a214f0cfc9714
JUNSEO PARK <pjs9177@naver.com>
2026-01-13 23:23:23 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 192 ms, Memory: 18408 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int[] count = new int[26];
+
+        for (int i = 0;i < s.length(); i++){
+            char c = s.charAt(i);
+            count[c - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++){
+            System.out.print(count[i] + " ");
+        }
+
+
+
+    }
+}
