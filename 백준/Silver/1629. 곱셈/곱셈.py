8368a57c95037f31965129251c542e9608f14e58
JUNSEO PARK <pjs9177@naver.com>
2026-01-21 00:26:13 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,18 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+A, B, C = map(int, input().split())
+
+def power(a,b):
+    if b == 1:
+        return a % C
+    
+    half = power(a, b // 2)
+    
+    if b % 2 == 0:
+        return (half * half) % C
+    
+    else:
+        return (half * half * a) % C
+
+print(power(A,B))
