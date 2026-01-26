4ae25a0b8180ec50cb8812a35829bd5fde636e48
JUNSEO PARK <pjs9177@naver.com>
2026-01-26 22:43:56 +0900

    [Silver IV] Title: 로프, Time: 2512 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+data = []
+
+for _ in range(n):
+    data.append(int(input()))
+
+data.sort()
+
+answer = 0
+
+for i in range(n):
+    answer = max(answer, data[i] * (n - i))
+
+print(answer)
