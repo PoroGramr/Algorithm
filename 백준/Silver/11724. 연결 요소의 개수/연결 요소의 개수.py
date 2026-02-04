9245c8d3ba734e287ae040ac3663e0b93dede8f0
JUNSEO PARK <pjs9177@naver.com>
2026-02-04 14:44:47 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 656 ms, Memory: 67048 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,38 +1,39 @@
 from collections import deque
 import sys
-
 input = sys.stdin.readline
+
 N,M = map(int, input().split())
 
-adj = [[] for _ in range(N)]
+graph = [[] for _ in range(N + 1)] 
+
+
+visited = [-1 for _ in range(N + 1)]
 
 for _ in range(M):
-  u,v = map(int, input().split())
-  adj[u-1].append(v-1)
-  adj[v-1].append(u-1)
+    a,b = map(int, input().split())
+    
+    graph[a].append(b)
+    graph[b].append(a)
 
+count  = 0
 
-visited = [False] * N
-count = 0
-
-for i in range(N):
-  if visited[i]:
-    continue
-  else:
-
-    count += 1
-
-    queue = deque()
-    queue.append(i)
-    visited[i] = True
-
-    while(queue):
-      u = queue.popleft()
-
-      for v in adj[u]:
-        if not visited[v]:
-          queue.append(v)
-          visited[v] = True
-
+for i in range(1, N + 1):
+    if visited[i] != -1:
+        continue
+    
+    else:
+        count += 1
+        
+        q = deque()
+        q.append(i)
+        visited[i] = 1
+        
+        while q:
+            c = q.popleft()
+            
+            for v in graph[c]:
+                if visited[v] == -1:
+                    q.append(v)
+                    visited[v] = 1
 
 print(count)
