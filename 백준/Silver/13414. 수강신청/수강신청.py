cacc9dd5ff9017affee68cc31d685ef464466f71
JUNSEO PARK <pjs9177@naver.com>
2026-02-06 19:54:41 +0900

    [Silver III] Title: 수강신청, Time: 372 ms, Memory: 63588 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -1,14 +1,15 @@
+import sys
+
+input = sys.stdin.readline
 K, L = map(int, input().split())
 
 data = {}
 
 for i in range(L):
-    number = input()
+    number = input().strip()
     data[number] = i
 
 sortStudent = sorted(data, key = lambda x : data[x])
 
 for i in range(0, min(K, len(sortStudent))):
     print(sortStudent[i])
-
-
