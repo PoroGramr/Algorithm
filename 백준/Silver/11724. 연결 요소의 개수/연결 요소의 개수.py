a33e0afbbe29ed069dfcab98ac60af1ab0ed3b76
JUNSEO PARK <pjs9177@naver.com>
2026-02-10 12:04:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 524 ms, Memory: 67152 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,39 +1,38 @@
-from collections import deque
 import sys
+
+from collections import deque
+
 input = sys.stdin.readline
 
 N,M = map(int, input().split())
 
-graph = [[] for _ in range(N + 1)] 
-
-
-visited = [-1 for _ in range(N + 1)]
+graph = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     a,b = map(int, input().split())
-    
     graph[a].append(b)
     graph[b].append(a)
 
-count  = 0
+visited = [-1 for _ in range(N + 1)]
+
+count = 0
 
 for i in range(1, N + 1):
     if visited[i] != -1:
         continue
-    
     else:
-        count += 1
-        
         q = deque()
         q.append(i)
-        visited[i] = 1
+        count += 1
         
         while q:
-            c = q.popleft()
-            
-            for v in graph[c]:
-                if visited[v] == -1:
-                    q.append(v)
-                    visited[v] = 1
+            cur = q.popleft()
+    
+            for nxt in graph[cur]:
+                if visited[nxt] == -1:
+                    q.append(nxt)
+                    visited[nxt] = 1
+
 
 print(count)
+            
