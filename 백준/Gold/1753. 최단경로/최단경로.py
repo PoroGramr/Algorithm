178ece179c437886a2e1bfc9c59f7676559a791a
JUNSEO PARK <pjs9177@naver.com>
2026-02-23 17:53:55 +0900

    [Gold IV] Title: 최단경로, Time: 340 ms, Memory: 135552 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -2,26 +2,26 @@ import sys
 import heapq
 
 input = sys.stdin.readline
-INF = 10**18
+INF = 10 ** 18
+
 
 V, E = map(int, input().split())
 K = int(input())
 
-graph = [[] for _ in range(V + 1)]
+graph = [[]for _ in range(V + 1)]
 for _ in range(E):
     u, v, w = map(int, input().split())
-    graph[u].append((v, w))  # u -> v 가중치 w
+    graph[u].append((v,w))
 
 dist = [INF] * (V + 1)
 dist[K] = 0
 
 pq = []
-heapq.heappush(pq, (0, K))  # (거리, 정점)
+heapq.heappush(pq, (0,K))
 
 while pq:
     cur_dist, u = heapq.heappop(pq)
-
-    # 힙에 남아있는 "옛날 값" 제거
+    
     if cur_dist != dist[u]:
         continue
 
@@ -29,7 +29,8 @@ while pq:
         nd = cur_dist + w
         if nd < dist[v]:
             dist[v] = nd
-            heapq.heappush(pq, (nd, v))
+            heapq.heappush(pq, (nd,v))
+        
 
 out = []
 for i in range(1, V + 1):
